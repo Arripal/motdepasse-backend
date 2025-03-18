ea883b8e836efa4936b46fbcfa55c76c174ac9be
Alban P <albanpirraud@hotmail.fr>
2025-03-18 15:53:09 +0100

    Personnalisation du message d'erreur lors d'une tentative de connexion infructueuse

---
+++ src/Security/ErrorLoginHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+
+class ErrorLoginHandler implements AuthenticationFailureHandlerInterface
+{
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        return new JsonResponse(
+            [
+                "message" => "Les identifiants ne sont pas valides. Veuillez vérifier votre saisie.",
+                "error" => $exception->getMessage(),
+            ],
+            JsonResponse::HTTP_UNAUTHORIZED
+        );
+    }
+}
